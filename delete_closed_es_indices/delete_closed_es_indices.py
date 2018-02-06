3e41ba2a7e9dbbb67fa08ccc0ec8500000c6e726
DennyZhang <contact@dennyzhang.com>
2018-02-06 15:37:57 -0600

    Update code

--- delete_closed_es_indices/delete_closed_es_indices.py
+++ delete_closed_es_indices/delete_closed_es_indices.py
@@ -9,12 +9,16 @@
 ## Description :
 ## --
 ## Created : <2018-02-06>
-## Updated: Time-stamp: <2018-02-06 15:26:02>
+## Updated: Time-stamp: <2018-02-06 15:37:43>
 ##-------------------------------------------------------------------
+# pip install elasticsearch
+import argparse
+import elasticsearch
 import sys
-def is_index_open(es_ip, es_port, index_name):
+################################################################################
+def detect_open_index(es_ip, es_port, index_name_list):
     # TODO
-    return False
+    return (False, [])
 
 def delete_index(es_ip, es_port, index_name):
     # TODO
@@ -24,10 +28,19 @@ def wait_es_slowness(es_ip, es_port, max_wait_seconds, try_count=3):
     # TODO
     return True
 
+################################################################################
 def delete_closed_index(es_ip, es_port, index_list, max_wait_seconds):
+    # precheck
+    (status, l) = detect_open_index(es_ip, es_port, index_name_list) is False:
+    if status:
+        print("ERROR: problematic input. Detected some open index: %s") % (','.join(l))
+        sys.exit(1)
+
+    # deal with each index
     for index_name in index_list:
         print("Delete index: %s" % (index_name))
-        if is_index_open(es_ip, es_port):
+        (status, l) = detect_open_index(es_ip, es_port, [index_name]):
+        if status:
             print("ERROR: index(%s) is open. Abort the whole process." % (index_name))
             sys.exit(1)
         if delete_index(es_ip, es_port, index_name) is False:
@@ -37,6 +50,17 @@ def delete_closed_index(es_ip, es_port, index_list, max_wait_seconds):
             sys.exit(1)
 
 if __name__ == '__main__':
-    max_wait_seconds = 5
-    delete_closed_index(es_ip, es_port, index_list, max_wait_seconds)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--es_ip', required=True, help="Elasticsearch IP", type=str)
+    parser.add_argument('--es_port', default='9200', help="Elasticsearch port", type=str)
+    parser.add_argument('--max_wait_seconds', dest='max_wait_seconds', default='5', \
+                        help="Wait for ES slowness after index removal")
+    # TODO
+    parser.add_argument('--index_list', required=True, default='mdm-master,mdm-staging',
+                        help="Index list to be deleted. If open index is detected, the whole process will abort", type=str)
+
+    l = parser.parse_args()
+    examine_only = l.examine_only
+    print "bucket_list: " + l.bucket_list
+    delete_closed_index(es_ip, es_port, index_list, l.max_wait_seconds)
 ## File: delete_closed_es_indices.py ends
