986a76d6a912122f0271f71dcff6932d69f901c6
DennyZhang <contact@dennyzhang.com>
2018-02-06 17:26:11 -0600

    pylint change

--- delete_closed_es_indices/delete_closed_es_indices.py
+++ delete_closed_es_indices/delete_closed_es_indices.py
@@ -9,18 +9,20 @@
 ## Description :
 ## --
 ## Created : <2018-02-06>
-## Updated: Time-stamp: <2018-02-06 15:45:46>
+## Updated: Time-stamp: <2018-02-06 17:25:58>
 ##-------------------------------------------------------------------
 # pip install elasticsearch==2.3.0
 import argparse
 import sys
 from elasticsearch import Elasticsearch
 ################################################################################
-def detect_open_index(es_instance, index_name_list):
-    # TODO
-    return (False, [])
+NOT_EXISTS="NOT_EXISTS"
+IS_CLOSED="IS_CLOSED"
+IS_OPEN="IS_OPEN"
 
-def delete_index(es_instance, index_name):
+def index_status(es_instance, index_name):
+    if es_instance.indices.exists(index=index_name) is False:
+        return NOT_EXISTS
     # TODO
     return False
 
@@ -32,20 +34,24 @@ def wait_es_slowness(es_instance, max_wait_seconds, try_count=3):
 def delete_closed_index(es_ip, es_port, index_list, max_wait_seconds):
     es_instance = Elasticsearch(["%s:%s"%(es_ip, es_port)])
     # precheck
-    (status, l) = detect_open_index(es_instance, index_name_list) is False:
-    if status:
-        print("ERROR: problematic input. Detected some open index: %s") % (','.join(l))
-        sys.exit(1)
+    for index_name in index_list:
+        status = index_status(es_instance, index_name)
+        if status != IS_CLOSED:
+            print("ERROR: index(%s) should be closed. But its status is %s" % (index_name, status))
+            sys.exit(1)
 
     # deal with each index
     for index_name in index_list:
         print("Delete index: %s" % (index_name))
-        (status, l) = detect_open_index(es_instance, [index_name]):
-        if status:
-            print("ERROR: index(%s) is open. Abort the whole process." % (index_name))
+        status = index_status(es_instance, index_name)
+        if status != IS_CLOSED:
+            print("ERROR: index(%s) should be closed. But its status is %s" % (index_name, status))
             sys.exit(1)
-        if delete_index(es_instance, index_name) is False:
-            print("ERROR: deleting index(%s) has failed" % (index_name))
+
+        output = es_instance.indices.delete(index=index_name)
+        if output != {'acknowledged': True}:
+            print("ERROR: deleting index(%s) has failed. output: %s" % (index_name, str(output)))
+
         if wait_es_slowness(es_instance, max_wait_seconds) is False:
             print("ERROR: ES is slow after deleting index(%s)." % (index_name))
             sys.exit(1)
@@ -63,5 +69,5 @@ if __name__ == '__main__':
     l = parser.parse_args()
     examine_only = l.examine_only
     print "bucket_list: " + l.bucket_list
-    delete_closed_index(es_ip, es_port, index_list, l.max_wait_seconds)
+    delete_closed_index(l.es_ip, l.es_port, l.index_list, l.max_wait_seconds)
 ## File: delete_closed_es_indices.py ends
