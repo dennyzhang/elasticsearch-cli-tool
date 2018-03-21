7c0857c34192dceefccd5b170204d9164d87591f
DennyZhang <contact@dennyzhang.com>
2018-03-21 00:25:03 -0500

    add more status check

--- check_es_gc_count.py
+++ check_es_gc_count.py
@@ -5,7 +5,7 @@
 ## Description :
 ## --
 ## Created : <2018-03-20>
-## Updated: Time-stamp: <2018-03-20 21:23:40>
+## Updated: Time-stamp: <2018-03-21 00:22:07>
 ##-------------------------------------------------------------------
 import sys
 import argparse, socket
@@ -25,11 +25,18 @@ def get_es_gc_count(es_host, es_port):
         res.append([nodes_dict[key]["name"], nodes_dict[key]["jvm"]["gc"]["collectors"]["old"]["collection_count"]])
     return sorted(res, key=lambda item: item[1], reverse=True)
 
-def check_es_gc_count(es_host, es_port):
+def check_es_gc_count(es_host, es_port, max_full_gc):
     l = get_es_gc_count(es_host, es_port)
+    failed_nodes = []
     print("ES nodes full gc, sorted in a reverse order")
     for [node_name, gc_count] in l:
         print("%s\t%s" % (node_name, gc_count))
+        if int(gc_count) >= max_full_gc:
+            failed_nodes.append(node_name)
+    if len(failed_nodes) != 0:
+        print("Error: below nodes have full gc more than %d: \n%s" % (max_full_gc, ','.join(failed_nodes)))
+        return False
+    return True
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -37,18 +44,21 @@ if __name__ == '__main__':
                         help="server ip or hostname for elasticsearch instance. Default value is ip of eth0", type=str)
     parser.add_argument('--es_port', default='9200', required=False, \
                         help="server port for elasticsearch instance", type=str)
+    parser.add_argument('--max_full_gc', default='300', required=False, type=int, \
+                        help="If some nodes have full gc more than this, fail the test")
     l = parser.parse_args()
     es_host = l.es_host
-    es_port = l.es_port
     # get ip of eth0, if es_host is not given
     if es_host is None:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         es_host = s.getsockname()[0]
 
+    ret = True
     try:
-        check_es_gc_count(es_host, es_port)
+        ret = check_es_gc_count(es_host, l.es_port, l.max_full_gc)
     except Exception as e:
         print("Unexpected error:%s, %s" % (sys.exc_info()[0], e))
         sys.exit(1)
+    if ret is False: sys.exit(1)
 ## File: check_es_gc_count.py ends
